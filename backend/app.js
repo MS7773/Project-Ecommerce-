f629dcead551f4cc7bb65968b70a4f707cfa9fe9
MS7773 <sethmonil30@gmail.com>
2024-09-25 09:44:00 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express') ;
+const colors = require('colors')
+const app = express() ;
+const PORT = 3000 ;
+const dbConnect = require('./db')
+const errorHandler  = require('./middleware/errorHandler')
+const cors=require('cors')
+//parse the data from the req.body
+app.use(express.json());
+
+dbConnect()
+app.use(cors())
+
+
+app.use('/api',require('./routes/userRoutes'))
+
+//global route handler 
+app.use((req,res,next)=>{
+    res.status(404).send(`requested url ${req.url} not found`)
+    next()
+})
+
+//global error handler middleware
+app.use(errorHandler) ;
+
+
+app.listen(PORT , ()=>{
+    console.log(colors.yellow(`App is listening on the PORT:${PORT}`))
+})
